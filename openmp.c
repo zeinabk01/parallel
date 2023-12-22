f680c958b89b236444a794257af582c9bfea1107
zeinabk01 <147296632+zeinabk01@users.noreply.github.com>
2023-12-22 12:13:06 +0200

    Update openmp.c

--- openmp.c
+++ openmp.c
@@ -2,79 +2,54 @@
 #include <stdlib.h>
 #include <omp.h>
 
-
 #define M 1024
 #define N 2048
 
-void matrixVectorMultiply(int *matrix, int *vector, int *result) {
+// Function to perform matrix-vector multiplication with ReLU activation in parallel using OpenMP
+void matrixVectorMultiplyWithReLU(int *matrix, int *vector, int *result) {
     #pragma omp parallel for
     for (int i = 0; i < M; i++) {
         result[i] = 0;
+
         for (int j = 0; j < N; j++) {
             result[i] += matrix[i * N + j] * vector[j];
         }
+
+        // Apply ReLU activation
+        result[i] = (result[i] > 0) ? result[i] : 0;
     }
 }
 
 int main() {
     double startTime, endTime;
 
-    // Perform matrix-vector multiplication in serial (non-parallel) mode
+    // Perform matrix-vector multiplication with ReLU activation in serial mode
     int *serialMatrix = (int *)malloc(M * N * sizeof(int));
     int *serialVector = (int *)malloc(N * sizeof(int));
     int *serialResult = (int *)malloc(M * sizeof(int));
 
-    // Initialize serialMatrix and serialVector (you may replace this with your own initialization logic)
+    // Initialize serialMatrix and serialVector 
     for (int i = 0; i < M * N; i++) {
-        serialMatrix[i] = 1;  // Example initialization
+        serialMatrix[i] = 1; 
     }
 
     for (int i = 0; i < N; i++) {
-        serialVector[i] = 1;  // Example initialization
+        serialVector[i] = 1;  
     }
 
-    startTime = omp_get_wtime();
+    startTime = omp_get_wtime(); // Start timing
 
-    // Perform matrix-vector multiplication in serial mode
-    matrixVectorMultiply(serialMatrix, serialVector, serialResult);
+    // Perform matrix-vector multiplication with ReLU activation in parallel using OpenMP
+    matrixVectorMultiplyWithReLU(serialMatrix, serialVector, serialResult);
 
-    endTime = omp_get_wtime();
-    double serialTime = endTime - startTime;  // Assign the value to serialTime
-    printf("Serial execution time: %f seconds\n", serialTime);
-
-    // Perform matrix-vector multiplication in parallel using OpenMP
-    int *parallelMatrix = (int *)malloc(M * N * sizeof(int));
-    int *parallelVector = (int *)malloc(N * sizeof(int));
-    int *parallelResult = (int *)malloc(M * sizeof(int));
-
-    // Initialize parallelMatrix and parallelVector (you may replace this with your own initialization logic)
-    for (int i = 0; i < M * N; i++) {
-        parallelMatrix[i] = 1;  // Example initialization
-    }
-
-    for (int i = 0; i < N; i++) {
-        parallelVector[i] = 1;  // Example initialization
-    }
-
-    startTime = omp_get_wtime();
-
-    // Perform matrix-vector multiplication in parallel using OpenMP
-    matrixVectorMultiply(parallelMatrix, parallelVector, parallelResult);
-
-    endTime = omp_get_wtime();
-    double parallelTime = endTime - startTime;  // Assign the value to parallelTime
+    endTime = omp_get_wtime(); // Stop timing
+    double parallelTime = endTime - startTime; // Calculate execution time
     printf("Parallel execution time: %f seconds\n", parallelTime);
-
-    
 
     // Cleanup
     free(serialMatrix);
     free(serialVector);
     free(serialResult);
-
-    free(parallelMatrix);
-    free(parallelVector);
-    free(parallelResult);
 
     return 0;
 }
