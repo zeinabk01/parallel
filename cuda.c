c5021df37b20f657b53fd20d5db2f1277fce0ce3
zeinabk01 <147296632+zeinabk01@users.noreply.github.com>
2023-12-22 12:40:17 +0200

    Update cuda.c

--- cuda.c
+++ cuda.c
@@ -1,88 +1,69 @@
-%%writefile cuda_code.cu
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/time.h>
+#include <time.h>
 
 #define M 1024
 #define N 2048
-#define BLOCK_SIZE 256
-#define TILE_SIZE 16  // Size of the matrix tile loaded into shared memory
+#define THREADS_PER_BLOCK 256
 
-__global__ void matrixVectorMultiply(int *matrix, int *vector, int *result, int m, int n) {
-    int tid = blockIdx.x * blockDim.x + threadIdx.x;
-    int stride = blockDim.x * gridDim.x;
+__global__ void matrixVectorMultiplyWithReLU(int *matrix, int *vector, int *result) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
 
-    __shared__ int sharedVector[BLOCK_SIZE];
-    __shared__ int sharedMatrixTile[TILE_SIZE][TILE_SIZE];
+    if (i < M) {
+        result[i] = 0;
 
-    // Load vector into shared memory
-    for (int i = threadIdx.x; i < n; i += blockDim.x) {
-        sharedVector[i] = vector[i];
-    }
-
-    __syncthreads();
-
-    // Process multiple elements per thread
-    for (int i = tid; i < m; i += stride) {
-        int sum = 0;
-
-        // Load matrix tile into shared memory
-        for (int j = threadIdx.x; j < TILE_SIZE; j += blockDim.x) {
-            sharedMatrixTile[j][threadIdx.x] = matrix[i * n + j * blockDim.x + threadIdx.x];
+        for (int j = 0; j < N; j++) {
+            result[i] += matrix[i * N + j] * vector[j];
         }
 
-        __syncthreads();
-
-        // Compute dot product using the matrix tile in shared memory
-        for (int j = 0; j < TILE_SIZE; j++) {
-            sum += sharedMatrixTile[j][threadIdx.x] * sharedVector[j];
-        }
-
-        // Store the result
-        result[i] = sum;
+        // Apply ReLU activation
+        result[i] = (result[i] > 0) ? result[i] : 0;
     }
 }
 
 int main() {
-    int *h_matrix, *h_vector, *h_result;
+    clock_t startTime, endTime;
+
+    int *parallelMatrix = (int *)malloc(M * N * sizeof(int));
+    int *parallelVector = (int *)malloc(N * sizeof(int));
+    int *parallelResult = (int *)malloc(M * sizeof(int));
+
+    // Initialize parallelMatrix and parallelVector
+    for (int i = 0; i < M * N; i++) {
+        parallelMatrix[i] = 1;
+    }
+
+    for (int i = 0; i < N; i++) {
+        parallelVector[i] = 1;
+    }
+
+    // Allocate device memory
     int *d_matrix, *d_vector, *d_result;
-
-    h_matrix = (int *)malloc(M * N * sizeof(int));
-    h_vector = (int *)malloc(N * sizeof(int));
-    h_result = (int *)malloc(M * sizeof(int));
-
-    // Initialize host matrix and vector
-
-    // ... (your existing code)
-
     cudaMalloc((void **)&d_matrix, M * N * sizeof(int));
     cudaMalloc((void **)&d_vector, N * sizeof(int));
     cudaMalloc((void **)&d_result, M * sizeof(int));
 
-    cudaMemcpy(d_matrix, h_matrix, M * N * sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_vector, h_vector, N * sizeof(int), cudaMemcpyHostToDevice);
+    // Copy data to device
+    cudaMemcpy(d_matrix, parallelMatrix, M * N * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_vector, parallelVector, N * sizeof(int), cudaMemcpyHostToDevice);
 
-    dim3 gridDim((M + BLOCK_SIZE - 1) / BLOCK_SIZE, 1, 1);
-    dim3 blockDim(BLOCK_SIZE, 1, 1);
+    startTime = clock();
 
-    struct timeval start, end;
-    gettimeofday(&start, NULL);
+    // Perform matrix-vector multiplication with ReLU activation using CUDA
+    matrixVectorMultiplyWithReLU<<<(M + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK, THREADS_PER_BLOCK>>>(
+        d_matrix, d_vector, d_result);
 
-    // Launch the kernel
-    matrixVectorMultiply<<<gridDim, blockDim>>>(d_matrix, d_vector, d_result, M, N);
-    cudaDeviceSynchronize();
+    // Copy result back to host
+    cudaMemcpy(parallelResult, d_result, M * sizeof(int), cudaMemcpyDeviceToHost);
 
-    gettimeofday(&end, NULL);
-
-    double parallelTime = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1.0e6;
-
+    endTime = clock();
+    double parallelTime = (double)(endTime - startTime) / CLOCKS_PER_SEC;
     printf("Parallel execution time: %f seconds\n", parallelTime);
 
-    // ... (your existing code)
-
-    free(h_matrix);
-    free(h_vector);
-    free(h_result);
+    // Cleanup
+    free(parallelMatrix);
+    free(parallelVector);
+    free(parallelResult);
     cudaFree(d_matrix);
     cudaFree(d_vector);
     cudaFree(d_result);
